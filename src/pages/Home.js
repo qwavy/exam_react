227d76a7927e6a39867f29b8a1939a4d5d871bc9
Lol 228 <nursultanr@gmail.com>
2022-09-13 20:57:42 +0600

    asd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,21 +29,31 @@ const Home = () => {
                 </div>
                 <h1 className='sale_h'>sale</h1>
                 <div className='content'>
-                    <div className='sale'>
-                        <p className='sale_p'>sale 20% wait you</p>
+                    {/* <div className='sale'> */}
+                        <div>
+                            <p className='sale_p'>sale 20% wait you</p>
+                        </div>
                         <div className='sale_png'>
                             <img src={sale_png} className='sale_png' />
-
                         </div>
-
-                    </div>
+                    {/* </div> */}
 
                 </div>
                 <div className='sale_male'>
-                    <div className='sale_male_png'>
-                        <img src={sale_male_png} className='sale_male_png' />
-                    </div>
+                    {/* <div className='sale'> */}
+                        <div className='sale_male_png'>
+                            <img src={sale_male_png} className='sale_male_png' />
+ 
+
+                        </div>
+                        <div className='sale_male_p'>
+                                <p className='sale_male_p'>sale 40% for male</p>
+                        </div>
+                    {/* </div> */}
                 </div>
+                <div className='contact'>
+                    
+                </div>   
             </div>
         </div>
     )
